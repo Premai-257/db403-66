680d6b7e5cff60d3eafdceace16ebefc5f7a37aa
Premai-257 <65111257@dpu.ac.th>
2023-10-10 12:00:22 +0700

    register signup

--- student_activity/connection.php
+++ student_activity/connection.php
@@ -4,3 +4,4 @@
         echo $conn ->connect_error;
         die ();
     }
+    
--- student_activity/signup.php
+++ student_activity/signup.php
@@ -1,12 +1,22 @@
-<?
- require 'connection.php';
+<?php
+    require 'connection.php';
+    if (isset ($_POST['submit'])){
+      $studentID = $_POST['studentID'];
+      $studentName = $_POST['studentName'];
+      $major = $_POST['major'];
+      $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+      $sql = "insert into 
+      student(studentID, studentName, majorID, password) values('{$studentID}', '{$studentName}', '{$major}', '{$password}')";
+      echo $sql;
+      $conn->query($sql);
+     }
 ?>
 <!doctype html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Student_Activity-Signup</title>
+    <title>student activity > Signup</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <style>
       html,
@@ -14,80 +24,68 @@
         height: 100%;
       }
 
-      .form-singup {
+      .form-signin {
         max-width: 330px;
         padding: 1rem;
       }
 
-      .form-singup .form-floating:focus-within {
+      .form-signin .form-floating:focus-within {
         z-index: 2;
       }
 
-      .form-singup input[type="email"] {
-        margin-bottom: -1px;
-        border-bottom-right-radius: 0;
-        border-bottom-left-radius: 0;
-      }
-
-      .form-singup input[type="password"] {
-        margin-bottom: 10px;
-        border-top-left-radius: 0;
-        border-top-right-radius: 0;
-      }
     </style>
+    <script>
+    function validate(){
+      let p1 = document.querySelector ('#password').value;
+      let p2 = document.querySelector ('#re-password').value;
+      if (p1 != p2){
+        alert('Passwords are not identical.')
+        event.preventDefault();
+      }
+    }      
+    </script>
   </head>
+
   <body class="d-flex align-items-center py-4 bg-body-tertiary">
-    <main class="form-singup w-100 m-auto">
-      <form>
-        <img class="mb-4" src="img/logo.png" alt="" width="333" height="auto">
+    <main class="form-signin w-100 m-auto">
+      <form action="signup.php" method="post" onsubmit="validate()">
+        <img class="mb-4" src="img/logo.png" alt="" width="130" height="">
+        <!-- <img src="..." class="card-img" alt="...">         -->
         <h1 class="h3 mb-3 fw-normal">Please sign up</h1>
-    
-        <div class="input-group mb-3">
-  <span class="input-group-text">ID</span>
-  <div class="form-floating">
-    <input type="text" class="form-control" id="Student-ID" placeholder="Username">
-    <label for="Student-ID">Student ID</label>
-  </div>
-</div>
-<div class="input-group mb-3">
-  <span class="input-group-text">Name</span>
-  <div class="form-floating">
-    <input type="text" class="form-control" id="Student-Name" placeholder="Username">
-    <label for="Student-Name">Student Name</label>
-  </div>
-</div>
-<div class="form-floating mb-2">
-          <select class="form-control" id="major">
+        <div class="form-floating mb-2">
+          <input required name="studentID" type="text" class="form-control" id="student_id" placeholder="">
+          <label for="student_id">Student ID</label>
+        </div>
+        <div class="form-floating mb-1">
+          <input required name="studentName" type="text" class="form-control" id="student_name" placeholder="">
+          <label for="student_name">Student Name</label>
+        </div>
+        <div class="form-floating mb-2">
+          <select name="major" class="form-control" id="major">
         <?php 
             $sql = 'select * from major order by faculty';
             $result = $conn->query($sql);
             while ($row = $result->fetch_assoc()) {
-                echo "<option value ='{$row['major']}'>
-                {$row['faculty']}-{$row['major']}
+                echo "<option value ='{$row['MajorID']}'>
+                {$row['Faculty']}-{$row['MajorName']}
                 </option>";
             }
             $conn->close();
         ?>
         </select>
-        <label for="major">major<label>
-</div>
-<div class="input-group mb-3">
-  <span class="input-group-text">Key</span>
-  <div class="form-floating">
-    <input type="text" class="form-control" id="Password" placeholder="Username">
-    <label for="Password">Password</label>
-  </div>
-</div>
-<div class="input-group mb-3">
-  <span class="input-group-text">Key</span>
-  <div class="form-floating">
-    <input type="text" class="form-control" id="Confirm-Password" placeholder="Username">
-    <label for="Confirm-Password">Confirm Password</label>
-  </div>
-</div>
-    
-        <button class="btn btn-primary w-100 py-2" type="submit">Sign up</button>
-        <!-- <p class="mt-5 mb-3 text-body-secondary">© 2017–2023</p> -->
+          <label for="major">majorID</label>
+        </div>
+        <div class="form-floating mb-2">
+          <input required name="password" type="password" class="form-control" id="password" placeholder="">
+          <label for="password">Password</label>
+        </div>
+        <div class="form-floating mb-2">
+          <input required type="password" class="form-control" id="re-password" placeholder="">
+          <label for="re-password">Retype-Password</label>
+        </div>
+        
+        <button name="submit" class="btn btn-primary w-100 py-2" type="submit">Sign up</button>
+        <p class="mt-5 mb-3 text-body-secondary">© 2017–2023</p>
       </form>
     </main>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
